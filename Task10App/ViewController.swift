eba0be3a2d004218f74d9819b22ccb491ea6c39c
KiyoshiYamazaki <take_my_time.23@icloud.com>
2021-04-18 13:11:02 +0900

    フィードバック反映→struct Prefecture自体にデータとしての存在意義を付与、バックカラー設定で想定のindexが返ってきた際にnilを返す。

--- Task10App/ViewController.swift
+++ Task10App/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak private var tableView: UITableView!
-    private let prefectures = Prefectures()
+    private let prefectures = Prefecture.prefectures
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,20 +22,20 @@ class ViewController: UIViewController {
 extension ViewController: UITableViewDataSource, UITableViewDelegate {
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return prefectures.prefectures.count
+        return prefectures.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
-        cell.textLabel?.text = prefectures.prefectures[indexPath.row]
+        cell.textLabel?.text = prefectures[indexPath.row].name
         cell.detailTextLabel?.text = "\(indexPath.row + 1)番目の都道府県です"
-        cell.backgroundColor = changeCellBackgroundColor(index: indexPath.row)
+        cell.backgroundColor = changeCellBackgroundColor(index: indexPath.row) ?? .white
 
         return cell
     }
 
-    private func changeCellBackgroundColor(index: Int) -> UIColor {
+    private func changeCellBackgroundColor(index: Int) -> UIColor? {
 
         let num = index % 3
 
@@ -47,7 +47,7 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
         case 2:
             return .green
         default:
-            return .white
+            return nil
         }
     }
 }
